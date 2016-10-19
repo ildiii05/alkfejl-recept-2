21fa57300aca3d508fa3d297791ce9c0bc86d206
Madák Ildikó <ildiii05@INF.ELTE.HU>
2016-10-19 15:33:33 +0200

    gyak7

--- app/Http/Controllers/RecipeController.js
+++ app/Http/Controllers/RecipeController.js
@@ -2,6 +2,8 @@
 
 const Database = use('Database')
 const Category = use('App/Model/Category')
+const Validator = use('Validator')
+const Recipe = use('App/Model/Recipe')
 
 
 class RecipeController {
@@ -13,13 +15,49 @@ class RecipeController {
         const categories = yield Category.all()
 
         for (let category of categories){
-            const topRecipies = yield category.recipes().lim(3).fetch
-            category.topRecipies = topRecipies.toJSON()
+            const topRecipies = yield category.recipes().limit(3).fetch();
+            category.topRecipes = topRecipies.toJSON()
         }
 
         yield response.sendView('main', {        
-             categories: categories.toJSON() 
+             categories: categories.toJSON()
         })
+    }
+
+    * create(request, response){
+        const categories = yield Category.all();
+
+        yield response.sendView('createRecipe', {
+            categories: categories.toJSON()
+        });
+    }
+
+    * doCreate(request, response){
+        const recipeData = request.except('_csrf');
+        const rules = {
+            name: 'required',
+            ingredients: 'required',
+            instructions: 'required',
+            category_id: 'required'
+        }
+
+        const validation = yield Validator.validateAll(recipeData, rules);
+        if(validation.fails()){
+            yield request
+                .withAll()
+                .andWith({ errors: validation.messages() })
+                .flash()
+
+            response.redirect('back')
+            return
+        }
+        yield Recipe.create(recipeData);
+        console.log(recipeData);
+        response.redirect('/');
+    }
+
+    *show(request, response){
+        yield response.sendView('showRecipe')
     }
 }
 
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -6,3 +6,7 @@ const Route = use('Route')
 //Route.on('/').render('main')
 
 Route.get('/', 'RecipeController.index')
+Route.get('/recipes/create', 'RecipeController.create')
+Route.post('/recipes/create', 'RecipeController.doCreate')
+
+Route.get('/recipes/:id', 'RecipeController.show')
